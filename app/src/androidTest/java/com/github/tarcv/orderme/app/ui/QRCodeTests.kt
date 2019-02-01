7f705b21cd8fa1e08875933873a6d33d51464535
Borys Gurtovyi <borys.gurtovyi@gotinder.com>
2019-01-31 20:45:49 -0800

    my first qr test

--- app/src/androidTest/java/com/github/tarcv/orderme/app/ui/QRCodeTests.kt
+++ app/src/androidTest/java/com/github/tarcv/orderme/app/ui/QRCodeTests.kt
@@ -1,7 +1,9 @@
 package com.github.tarcv.orderme.app.ui
 
 import android.support.test.espresso.Espresso.onView
-import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.action.ViewActions.*
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
 import android.support.test.espresso.matcher.ViewMatchers.withId
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
@@ -15,12 +17,29 @@ class QRCodeTests {
 
     @get:Rule
     var mActivityTestRule = ActivityTestRule(SplashActivity::class.java)
-
+//asd
     @Test
     fun qrCodeFromList() {
         onView(withId(R.id.login_later_button))
                 .perform(click())
+
+        onView(withId(R.id.searchBtn))
+                .perform(click())
+
+        val t = "3_5"
+
+        onView(withId(R.id.qrCodeText))
+                .perform(replaceText(t), closeSoftKeyboard())
+
+        onView(withId(R.id.submitButton))
+                .perform(click())
+
+        onView(withId(R.id.restaurant_options_recycler))
+                .check(matches(isDisplayed()))
     }
 
 
+
 }
+
+
