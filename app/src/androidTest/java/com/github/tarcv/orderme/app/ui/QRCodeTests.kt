c28a5063d3404dd2e94ebb294318204125da5af4
Tagir Fakhriev <tagir@Tagirs-MacBook-Pro-2.local>
2019-02-03 14:13:35 -0800

    Fix Wildcard import (cannot be auto-corrected)

--- app/src/androidTest/java/com/github/tarcv/orderme/app/ui/QRCodeTests.kt
+++ app/src/androidTest/java/com/github/tarcv/orderme/app/ui/QRCodeTests.kt
@@ -5,7 +5,7 @@ import android.support.test.espresso.action.ViewActions.click
 import android.support.test.espresso.action.ViewActions.replaceText
 import android.support.test.espresso.action.ViewActions.closeSoftKeyboard
 import android.support.test.espresso.assertion.ViewAssertions.matches
-import android.support.test.espresso.matcher.ViewMatchers.*
+import android.support.test.espresso.matcher.ViewMatchers.withId
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 import com.github.tarcv.orderme.app.R
