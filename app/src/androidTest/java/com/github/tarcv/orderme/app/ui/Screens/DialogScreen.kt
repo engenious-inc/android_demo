73411006ebfe4d588e9380a59dc1dba7e9d4247b
gokhanozturk <gokhan.inbox@gmail.com>
2019-02-05 22:36:01 +0300

    simplified the imported viewmatchers

--- app/src/androidTest/java/com/github/tarcv/orderme/app/ui/Screens/DialogScreen.kt
+++ app/src/androidTest/java/com/github/tarcv/orderme/app/ui/Screens/DialogScreen.kt
@@ -2,15 +2,16 @@ package com.github.tarcv.orderme.app.ui.Screens
 
 import android.support.test.espresso.Espresso
 import android.support.test.espresso.ViewInteraction
-import android.support.test.espresso.assertion.ViewAssertions
-import android.support.test.espresso.matcher.ViewMatchers
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
+import android.support.test.espresso.matcher.ViewMatchers.withText
 
 class DialogScreen {
     private val errorMessage: (String) -> ViewInteraction = { errorMessage ->
-        Espresso.onView(ViewMatchers.withText(errorMessage))
+        Espresso.onView(withText(errorMessage))
     }
 
     fun isErrorDisplayed(errorMessage: String) {
-        errorMessage(errorMessage).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        errorMessage(errorMessage).check(matches(isDisplayed()))
     }
 }
