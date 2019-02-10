877ef699faa4d1b40bb20212a1c0b02ee31143ea
gokhanozturk <gokhan.inbox@gmail.com>
2019-02-11 00:23:22 +0300

    added init block to check the proper GUI elements are displayed

--- app/src/androidTest/java/com/github/tarcv/orderme/app/ui/Screens/HomeScreen.kt
+++ app/src/androidTest/java/com/github/tarcv/orderme/app/ui/Screens/HomeScreen.kt
@@ -3,8 +3,10 @@ package com.github.tarcv.orderme.app.ui.Screens
 import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.ViewInteraction
 import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.matcher.ViewMatchers.withId
 import android.support.test.espresso.matcher.ViewMatchers.withText
+import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
 import com.github.tarcv.orderme.app.R
 
 class HomeScreen {
@@ -13,6 +15,10 @@ class HomeScreen {
 
     private val restaurantCell: (String) -> ViewInteraction = { restaurantName ->
         onView(withText(restaurantName))
+    }
+
+    init {
+        searchButton.check(matches(isDisplayed()))
     }
 
     fun search(): MockQRCodeScreen {
--- app/src/androidTest/java/com/github/tarcv/orderme/app/ui/Screens/LoginScreen.kt
+++ app/src/androidTest/java/com/github/tarcv/orderme/app/ui/Screens/LoginScreen.kt
@@ -3,12 +3,18 @@ package com.github.tarcv.orderme.app.ui.Screens
 import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.ViewInteraction
 import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
 import android.support.test.espresso.matcher.ViewMatchers.withId
 import com.github.tarcv.orderme.app.R
 
 class LoginScreen {
     private val loginLaterButton: ViewInteraction
         get() = onView(withId(R.id.login_later_button))
+
+    init {
+        loginLaterButton.check(matches(isDisplayed()))
+    }
 
     fun loginLater(): HomeScreen {
         loginLaterButton.perform(click())
