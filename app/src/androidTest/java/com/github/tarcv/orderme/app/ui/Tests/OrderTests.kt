0a07ae35f587f943489079551c2a818f033cabe0
Borys Gurtovyi <borys.gurtovyi@gotinder.com>
2019-02-21 20:36:58 -0800

    siblings and parents

--- app/src/androidTest/java/com/github/tarcv/orderme/app/ui/Tests/OrderTests.kt
+++ app/src/androidTest/java/com/github/tarcv/orderme/app/ui/Tests/OrderTests.kt
@@ -2,14 +2,14 @@ package com.github.tarcv.orderme.app.ui.Tests
 
 import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.action.ViewActions.click
-import android.support.test.espresso.matcher.ViewMatchers.withId
-import android.support.test.espresso.matcher.ViewMatchers.withText
+import android.support.test.espresso.matcher.ViewMatchers.*
 import android.support.test.rule.ActivityTestRule
 import com.github.tarcv.orderme.app.R
 import com.github.tarcv.orderme.app.ui.Screens.LoginScreen
 import com.github.tarcv.orderme.app.ui.Screens.RestaurantScreen
 import com.github.tarcv.orderme.app.ui.SplashActivity
 import com.github.tarcv.orderme.app.ui.Utilities.getText
+import org.hamcrest.Matchers.allOf
 import org.junit.Assert
 import org.junit.Rule
 import org.junit.Test
@@ -24,6 +24,15 @@ class OrderTests {
     val validQR = "3_5"
     val restaurantName = "Republique"
     val errorMessage = "QR Code could not be scanned"
+
+    fun addToCart(dish: String) {
+        onView(
+                allOf(
+                        withId(R.id.plus_button),
+                        isDescendantOfA(hasSibling(withText(dish)))
+                )
+        ).perform(click())
+    }
 
     @Test
     fun testCustomAction() {
@@ -49,5 +58,32 @@ class OrderTests {
         onView(withId(R.id.bucket_textview))
                 .perform(getText(bucketTextHolder))
         Assert.assertEquals(bucketTextHolder.get(), "0.0")
+    }
+
+    @Test
+    fun clickOnPlusButton() {
+        val loginScreen = LoginScreen()
+        val homeScreen = loginScreen.loginLater()
+
+        val qrCodeScreen = homeScreen.search()
+        qrCodeScreen.enterQRCode(validQR)
+                .submit()
+
+        val restaurantScreen = RestaurantScreen()
+
+        sleep(1000)
+
+        onView(withText("Menu"))
+                .perform(click())
+        sleep(1000)
+
+        onView(withText("SALADS AND VEGETABLES"))
+                .perform(click())
+        sleep(1000)
+
+
+        addToCart("OCTOPUS")
+        addToCart("OCTOPUS")
+        addToCart("OCTOPUS")
     }
 }
