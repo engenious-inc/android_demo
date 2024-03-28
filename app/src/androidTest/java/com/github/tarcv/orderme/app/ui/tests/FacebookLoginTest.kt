6aac1f819545ac4f23b3730030430af01ebea768
vadympastushok <vadym.pastushok@gotinder.com>
2024-03-28 10:37:50 +0100

    test commit

--- app/src/androidTest/java/com/github/tarcv/orderme/app/ui/tests/FacebookLoginTest.kt
+++ app/src/androidTest/java/com/github/tarcv/orderme/app/ui/tests/FacebookLoginTest.kt
@@ -13,6 +13,7 @@ class FacebookLoginTest : BaseTest() {
     @Test
     fun testFacebookLogin() {
         login {
+
             mockFBLogin()
             tapDeviceBackButton()
             checkContinueButtonPresent()
