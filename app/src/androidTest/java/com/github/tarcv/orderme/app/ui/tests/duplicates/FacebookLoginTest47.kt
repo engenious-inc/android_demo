30cfd62618f21f9486a050fcc079c9edc0ba8cc3
vadympastushok <vadym.pastushok@gotinder.com>
2024-04-08 13:35:52 +0200

    created more duplicates of instrumented tests

---
+++ app/src/androidTest/java/com/github/tarcv/orderme/app/ui/tests/duplicates/FacebookLoginTest47.kt
@@ -0,0 +1,21 @@
+package com.github.tarcv.orderme.app.ui.tests
+
+import androidx.test.rule.ActivityTestRule
+import com.github.tarcv.orderme.app.ui.SplashActivity
+import com.github.tarcv.orderme.app.ui.robots.login
+import org.junit.Rule
+import org.junit.Test
+
+class FacebookLoginTest47 : BaseTest() {
+    @get:Rule
+    val mActivityTestRule = ActivityTestRule(SplashActivity::class.java)
+
+    @Test
+    fun testFacebookLogin() {
+        login {
+            mockFBLogin()
+            tapDeviceBackButton()
+            checkContinueButtonPresent()
+        }
+    }
+}
---
+++ app/src/androidTest/java/com/github/tarcv/orderme/app/ui/tests/duplicates/RestaurantListTest14.kt
@@ -0,0 +1,79 @@
+package com.github.tarcv.orderme.app.ui.tests
+
+import androidx.test.rule.ActivityTestRule
+import com.github.tarcv.orderme.app.ui.SplashActivity
+import com.github.tarcv.orderme.app.ui.robots.login
+import com.github.tarcv.orderme.app.ui.robots.qrCode
+import com.github.tarcv.orderme.app.ui.robots.restaurant
+import com.github.tarcv.orderme.app.ui.robots.restaurantList
+import com.github.tarcv.orderme.app.ui.robots.menuCategories
+import com.github.tarcv.orderme.app.ui.robots.bucket
+import junit.framework.TestCase.assertEquals
+import org.junit.Assert.assertNotSame
+
+import org.junit.Rule
+import org.junit.Test
+
+class RestaurantListTest14 : BaseTest() {
+    @get:Rule
+    val mActivityTestRule = ActivityTestRule(SplashActivity::class.java)
+
+    private var numberOfRestPresent: Int = 1
+    private var cartTotal: Double = 0.0
+    private val numberOfRestaurants = 6
+
+    @Test
+    fun verifySearchTest() {
+        setupMockData()
+        login {
+            loginLater()
+        }
+
+        restaurantList {
+            typeTextInSearchField(romanov)
+            assertNotSame(hakkasan, getRestaurantTitleText())
+            checkNumberOfRestaurants(numberOfRestPresent)
+            assertEquals(romanov, getRestaurantTitleText())
+        }
+    }
+    @Test
+    fun checkNumberOfRestaurants() {
+        setupMockData()
+        login {
+            loginLater()
+        }
+
+        restaurantList {
+            checkNumberOfRestaurants(numberOfRestaurants)
+        }
+    }
+
+    @Test
+    fun checkDefaultCartValue() {
+        setupMockData()
+        login {
+            loginLater()
+        }
+
+        restaurantList {
+            tapOnQrCodeButton()
+        }
+
+        qrCode {
+            typeInNewQrCode(republiqueQR)
+            tapOnSubmitButton()
+        }
+
+        restaurant {
+            selectMenuOption()
+        }
+
+        menuCategories {
+            selectFish()
+        }
+
+        bucket {
+            assertEquals(cartTotal, getBucketTotal())
+        }
+    }
+}
