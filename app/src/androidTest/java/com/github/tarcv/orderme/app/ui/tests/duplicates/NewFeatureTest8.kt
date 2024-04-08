93b62088607de10e22312b2a4ec5a73617fe1658
vadympastushok <vadym.pastushok@gotinder.com>
2024-04-08 13:58:34 +0200

    created 420 duplicates of tests

---
+++ app/src/androidTest/java/com/github/tarcv/orderme/app/ui/tests/duplicates/NewFeatureTest8.kt
@@ -0,0 +1,22 @@
+package com.github.tarcv.orderme.app.ui.tests.duplicates
+
+import com.github.tarcv.orderme.app.ui.tests.BaseTest
+import androidx.test.rule.ActivityTestRule
+import com.github.tarcv.orderme.app.ui.SplashActivity
+import com.github.tarcv.orderme.app.ui.robots.login
+import org.junit.Rule
+import org.junit.Test
+
+class NewFeatureTest8 : BaseTest() {
+    @get:Rule
+    val mActivityTestRule = ActivityTestRule(SplashActivity::class.java)
+
+    @Test
+    fun testFacebookLogin() {
+        login {
+            mockFBLogin()
+            tapDeviceBackButton()
+            checkContinueButtonPresent()
+        }
+    }
+}
---
+++ app/src/androidTest/java/com/github/tarcv/orderme/app/ui/tests/duplicates/OrderTest47.kt
@@ -0,0 +1,123 @@
+package com.github.tarcv.orderme.app.ui.tests.duplicates
+
+import com.github.tarcv.orderme.app.ui.tests.BaseTest
+import androidx.test.rule.ActivityTestRule
+import com.github.tarcv.orderme.app.ui.SplashActivity
+import com.github.tarcv.orderme.app.ui.robots.restaurantList
+import com.github.tarcv.orderme.app.ui.robots.restaurant
+import com.github.tarcv.orderme.app.ui.robots.qrCode
+import com.github.tarcv.orderme.app.ui.robots.menuItem
+import com.github.tarcv.orderme.app.ui.robots.menuCategories
+import com.github.tarcv.orderme.app.ui.robots.bucket
+import com.github.tarcv.orderme.app.ui.robots.orders
+import com.github.tarcv.orderme.app.ui.robots.login
+import com.github.tarcv.orderme.app.ui.robots.reservation
+import com.github.tarcv.orderme.app.ui.robots.reservations
+import com.github.tarcv.orderme.app.ui.robots.popUp
+import org.junit.Ignore
+import org.junit.Rule
+import org.junit.Test
+
+class OrderTest47 : BaseTest() {
+    @get:Rule
+    val mActivityTestRule = ActivityTestRule(SplashActivity::class.java)
+
+    private val comment = "San Jose"
+    private val phoneNumber = "1111111"
+    private val numberOfPeople = "4"
+    private val orderDate = "2020-04-27"
+    private val time = "02:04"
+    private val bucketTotal = "77.0"
+    private val reservationDate = "2020-06-27"
+    private val restaurantName = "Republique"
+
+    @Ignore
+    @Test
+    fun completeOrderFlowForRepublique() {
+        setupMockData()
+        login {
+            mockFBLogin()
+        }
+
+        restaurantList {
+            tapOnQrCodeButton()
+        }
+
+        qrCode {
+            typeInNewQrCode(republiqueQR)
+            tapOnSubmitButton()
+        }
+
+        restaurant {
+            tapOnMenuOption()
+        }
+
+        menuCategories {
+            tapSaladsAndVegetables()
+        }
+
+        menuItem {
+            tapOnPlusButton(colemanFarmsLittleGems)
+            tapOnPlusButton(blackAndWhiteSalad)
+            tapOnPlusButton(octopus)
+            tapOnShoppingCart()
+        }
+
+        bucket {
+            typeInComments(comment)
+            tapOnAccept()
+        }
+
+        popUp {
+            checkOrderStatus()
+            tapOkButton()
+        }
+
+        restaurant {
+            tapOnOrders()
+        }
+
+        orders {
+            checkOrderIsDisplayed(restaurantName, orderDate, time, bucketTotal)
+        }
+    }
+
+//    @Test
+    fun restaurantReservationFlow() {
+        setupMockData()
+        login {
+            mockFBLogin()
+        }
+
+        restaurantList {
+            tapOnQrCodeButton()
+        }
+
+        qrCode {
+            typeInNewQrCode(republiqueQR)
+            tapOnSubmitButton()
+        }
+
+        restaurant {
+            tapOnReservation()
+        }
+
+        reservation {
+            typeNumberOfPeople(numberOfPeople)
+            typePhoneNumber(phoneNumber)
+            selectDate(reservationDate)
+            tapBookButton()
+        }
+
+        popUp {
+            checkReservationSuccessMessage()
+            tapOkButton()
+        }
+
+        reservations {
+            tapOnReservations()
+            tapFutureReservations()
+            isReservationDisplayed(restaurantName, reservationDate, time)
+        }
+    }
+}
